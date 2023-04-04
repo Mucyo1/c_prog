41e57e7d5dd91990cbfe45c702b420c01a57bd51
Mucyo prince herve <89136880+Mucyo1@users.noreply.github.com>
2023-04-04 14:05:05 +0200

    Update helloworld.c
    My first hello world

--- helloworld.c
+++ helloworld.c
@@ -5,7 +5,10 @@
 
 int main()
 {
+    int x;
     printf("hello world!\n"); //'\n' prints a new line after the code is execute
-    printf("this is my first printf program!\n");//the semicolon(;) indicates the end of the programming line
+    printf("what is your name?");
+    scanf("%s", x);
+    printf("Hey %s this is my first printf program!\n", x);//the semicolon(;) indicates the end of the programming line
         return (0);
 }
