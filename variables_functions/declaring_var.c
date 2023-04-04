705de857b931b83467afc7ab88887507d0085cec
Mucyo prince Herve <mucyoprinceherve@gmail.com>
2023-04-04 11:52:08 +0000

    types of variables

--- variables_functions/declaring_var.c
+++ variables_functions/declaring_var.c
@@ -6,14 +6,17 @@
     int y = 5; // intialization + declaration
     
     int age = 21; //integer holds decimals
-    float gpa = 2.5;//float holds fractional numbers on the other hand a double 
+    float gpa = 2.5;//float holds fractional numbers 
     char grade = 'A';//char holds single character 
     char name[] = "Marry";//this char will hold a certain combination of characters or strings through the use of []
+    float ga = 2.5;
     
     printf("my name is %s\n", name);
     printf("my age is %d\n", age);
-    printf("my gpa is %f\n", gpa);
+    printf("my gpa is %f\n", gpa);// a double datatype uses %lf
     printf("my grades are %c\n", grade);
+    printf("this is called the format specifier %.2f \n", ga);
+    printf("this is different from %f \n", ga);
     
     return (0);
  }
