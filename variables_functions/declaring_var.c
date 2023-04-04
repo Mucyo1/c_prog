7c8cc90b3aa476cd6f42e78dd41ef320a378d0fc
Mucyo prince Herve <mucyoprinceherve@gmail.com>
2023-04-04 11:43:00 +0000

    types of variables

--- variables_functions/declaring_var.c
+++ variables_functions/declaring_var.c
@@ -6,7 +6,7 @@
     int y = 5; // intialization + declaration
     
     int age = 21; //integer holds decimals
-    float gpa = 2.5;//float holds fractional numbers
+    float gpa = 2.5;//float holds fractional numbers on the other hand a double 
     char grade = 'A';//char holds single character 
     char name[] = "Marry";//this char will hold a certain combination of characters or strings through the use of []
     
