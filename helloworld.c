745689c0053679a38612bea50ebac22ad1fecc56
Mucyo prince Herve <mucyoprinceherve@gmail.com>
2023-04-04 10:36:14 +0000

    hello world

---
+++ helloworld.c
@@ -0,0 +1,11 @@
+/**
+ * this program shows how to output a message using printf 
+ */
+#include <stdio.h>
+
+int main()
+{
+    printf("hello world!\n"); //'\n' prints a new line after the code is execute
+    printf("this is my first printf program!\n");//the semicolon(;) indicates the end of the programming line
+        return (0);
+}
