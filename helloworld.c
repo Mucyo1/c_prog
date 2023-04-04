f8a18978e9644549afd180b6aa1e14bb3aa3280b
Mucyo prince Herve <mucyoprinceherve@gmail.com>
2023-04-04 12:15:03 +0000

    This is my first hello world

--- helloworld.c
+++ helloworld.c
@@ -5,10 +5,14 @@
 
 int main()
 {
-    int x;
+    char name[10] = " ";// 10n sets the number of strings to be obtained as name
+    
     printf("hello world!\n"); //'\n' prints a new line after the code is execute
-    printf("what is your name?");
-    scanf("%s", x);
-    printf("Hey %s this is my first printf program!\n", x);//the semicolon(;) indicates the end of the programming line
+    
+    printf("what is your name? \n");
+    scanf("%s", name);
+    
+    printf("Hey %s this is my first printf program!\n", name);//the semicolon(;) indicates the end of the programming line
+        
         return (0);
 }
