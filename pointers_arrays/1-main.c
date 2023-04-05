5a00e52506d32001302059664fa9ef6424c83ca9
Mucyo prince Herve <mucyoprinceherve@gmail.com>
2023-04-05 16:58:20 +0000

    printing variable's address

--- pointers_arrays/1-main.c
+++ pointers_arrays/1-main.c
@@ -9,6 +9,7 @@ int main(void)
 {
    char c;
    int n;
+   //it's important to remember that the variable's address can't be controlled therefore it changes every time
 
    printf("Address of variable 'c': %p\n", &c);//& sign is used to print the address of c.
    printf("Address of variable 'n': %p\n", &n);//%p prints the value of pointers with printf.
