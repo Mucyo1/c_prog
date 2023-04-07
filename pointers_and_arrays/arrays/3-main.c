76d4641731b9764e06779315eee1a1cc98ce8024
Mucyo prince Herve <mucyoprinceherve@gmail.com>
2023-04-07 11:31:46 +0000

    An array is not a pointer, but...

--- pointers_and_arrays/arrays/3-main.c
+++ pointers_and_arrays/arrays/3-main.c
@@ -8,9 +8,13 @@
 int main(void)
 {
    int a[98]; /* array */
+   int name[10];
 
    printf("a: %p\n", a);
    printf("&a[0]: %p\n", &a[0]);
+   
+   printf("value of name: %d\n", name[10]);
+   printf("value of name: %p\n", name);
    return (0);
 }
 //An array is not a pointer.
