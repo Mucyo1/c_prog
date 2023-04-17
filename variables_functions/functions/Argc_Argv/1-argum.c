bacee059f2a4c23548680f4c27d9a44f3553ac85
Mucyo prince Herve <mucyoprinceherve@gmail.com>
2023-04-17 12:18:40 +0000

    a program that prints its name, followed by a new line and the number of arguments passed into it

---
+++ variables_functions/functions/Argc_Argv/1-argum.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[])
+{
+    printf("argc is: %d\n", argc);//this command will print the the number of arguments passed after execution.
+    printf("argv is: %s\n", argv[0]);//this command will print the name of the executable file.
+    
+    
+}
