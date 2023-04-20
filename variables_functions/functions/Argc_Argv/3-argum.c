c93dcb0faf8c0664479dafb1b709aaafc690245e
Mucyo prince Herve <mucyoprinceherve@gmail.com>
2023-04-20 11:26:53 +0000

    function that passes arguments

--- variables_functions/functions/Argc_Argv/3-argum.c
+++ variables_functions/functions/Argc_Argv/3-argum.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+#include <stddef.h>
 
 int main(int argc, char* argv[])
 {
     int i;
-    int sum;
+    printf("the argc is :%d\n", argc);
+    printf("the argv is :%s\n", argv[0]);
     
-    if (argc > 1)
+    for (i = 0; i < argc; i++)
     {
-        for (i = 0; i < argc; i++)
-        {
-            printf("argv[%d] is = %s\n", i, argv[i]);
-            sum = sum + atoi(argv[i]);//this line will add the argument vectors
-        }
-        printf (" the total is = %d", sum);
-        }
-    return (0);
+        printf("the argument passed are argv[%d] : %s\n", i, argv[i]);
+    }
 }
+
+
+
+
