fd3647fa5b894dd12c2319fe82bd3389adda53f2
Mucyo prince Herve <mucyoprinceherve@gmail.com>
2023-04-21 11:14:54 +0000

    introduction to macros

---
+++ Pre_Processor_Directives/pre.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+ #define TABLESIZE BUFSIZE
+     #define BUFSIZE 1024
+
+
+int main()
+{
+    printf("macro TABLESIZE holds %d\n", TABLESIZE);
+    printf("macro %d\n", BUFSIZE);
+}
