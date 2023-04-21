4db8cdc174d4dec16da92d97fbb13e042144eb40
Mucyo prince Herve <mucyoprinceherve@gmail.com>
2023-04-21 09:49:49 +0000

    Program that dispays number stored in the allocated memory

---
+++ pointers_and_arrays/malloc/3-mall.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    int *a, i;
+
+    a = malloc(sizeof(*a)*10);
+
+    for(i = 0; i < 10; i++) a[i] = 10 - i;
+    for (i = 0; i < 10; i++)
+    {
+        printf("a [%d] = %d\n", i, a[i]);
+    }
+}
