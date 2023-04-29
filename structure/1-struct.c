4b45152db72afb5073aba68df27a51722d54702e
Mucyo prince Herve <mucyoprinceherve@gmail.com>
2023-04-29 11:19:03 +0000

    Declaring structures

--- structure/1-struct.c
+++ structure/1-struct.c
@@ -1,1 +1,26 @@
-c
+#include <stdio.h>
+#include <stdlib.h>
+
+//declaring a structure
+//we create a struct that holds info of a student.
+
+struct studentinfo
+    {
+        char *name;
+        char *email;
+        int age;
+    };
+
+int main (void)
+{
+    struct studentinfo student;
+    student.name = "tech monster";
+    student.email = "mucyoprinceherve@gmail.com";
+    student.age = 20;
+
+    printf("student's name: %s\n", student.name);
+    printf("student's email: %s\n ", student.email);
+    printf("student's age is: %d\n", student.age);
+
+    return 0;
+}
--- structure/2-struct.c
+++ structure/2-struct.c
@@ -4,30 +4,47 @@
 //declaring a structure
 //we create a struct that holds info of a student.
 
-struct studentinfo
+struct studentinfo 
+// studentinfo is a structure tap or type it helps create different
+// structure names with same stucture members
     {
+        //these are structure members
         char *name;
         char *email;
         int age;
-    }student1, student2;
+    };
 
-int main (void)
+int manager()
 {
-    struct studentinfo ;
+    char title[] = "kageyo";
+
+    printf("staff manager is: %s", title);
+
+    return 0;
+}
+
+int main ()
+{
+
+    struct studentinfo student1, student2 ;
+    // student1 and student2 are structure names with same struct members
     student1.name = "Mucyo Prince Herve";
     student1.email = "mucyoprinceherve@gmail.com";
     student1.age = 20;
-    
+
     //**----------------***-----------------*//
-    
+
     student2.name = "tech monster";
     student2.email = "techmonster@gmail.com";
     student2.age = 25;
-    
+
+
+
+    printf("%d\n\n", manager());
     printf("student's name: %s\n", student1.name);
     printf("student's email: %s\n", student1.email);
     printf("student's age is: %d\n\n", student1.age);
-    
+
     printf("student's name: %s\n", student2.name);
     printf("student's email: %s\n", student2.email);
     printf("student's age is: %d\n", student2.age);
