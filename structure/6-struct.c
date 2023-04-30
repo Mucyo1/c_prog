7be23e3de353831cf736a293215cce9e03135c21
Mucyo prince Herve <mucyoprinceherve@gmail.com>
2023-04-30 11:10:16 +0000

    Pointer to Structure Variable

--- structure/6-struct.c
+++ structure/6-struct.c
@@ -14,8 +14,9 @@ int main (void)
 {
     struct abc a = {0, 1};
     struct abc *ptr = &a;
-
-    printf("%d %d\n",ptr->x,ptr->y);
-    //(*ptr).x is same as ptr->x it all state that pointer ptr points to y and 
+    //all these ways of out put are applicable depending on the programmer
+    //(*((int*)ptr+1),ptr->y, (*ptr).y) they all got the same output
+    printf("%d %d %d\n",*((int*)ptr+1),ptr->y, (*ptr).y);
+    //(*ptr).x is same as ptr->x it all state that pointer ptr points to y and
     return 0;
 }
