9a73880882c07fe68633edf323939704e8d72ab9
Mucyo prince Herve <mucyoprinceherve@gmail.com>
2023-04-30 22:10:16 +0000

    first time with single linked list

---
+++ structure/7-struct.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//creating a  struct node.
+
+struct structnode
+    {
+        // a node is made up of data and link to the next node.
+        int data;
+        struct structnode *next;
+    };
+
+
+void printList(struct structnode* n)
+{
+    while (n != NULL) {
+        printf(" %d ", n->data);
+        n = n->next;
+    }
+    printf("\n");
+}
+
+
+int main()
+{
+    //we declare two node
+    struct structnode *head = NULL;
+    struct structnode *second = NULL;
+
+    // we create memory space for the nodes
+    head = malloc(sizeof(struct structnode));
+    second = malloc(sizeof(struct structnode));
+
+    head->data = 45;// assigns data to the first node
+    head->next = second;//link first node to the second
+
+    second->data = 46;
+    second->next = NULL; //as second is the last node it will point to null
+
+    printList(head);
+
+    return 0;
+//head->next
+//next will contain the link to the next node called second
+// head->next->next will contain NULL as it's the end of the node.
+
+
+
+}
