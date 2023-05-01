1574bb3375762057d82748a5a24fcd2f716b5550
Mucyo prince Herve <mucyoprinceherve@gmail.com>
2023-05-01 14:26:51 +0000

    Traversing a Single Linked List (Counting the Nodes)

---
+++ structure/8-struct.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//creating a  struct node.
+
+struct structnode
+    {
+        // a node is made up of data and link to the next node.
+        int data;
+        struct structnode *next;
+    };
+
+void count_of_nodes(struct structnode *head)
+{
+    int count = 0;
+
+    if (head == NULL)
+    {
+        printf("the node is empty");
+    }
+
+    struct structnode *ptr = head;
+    while(ptr != NULL)
+    {
+        count++;
+        ptr = ptr->next;
+    }
+    printf("the number of nodes is : %d\n", count);
+}
+
+
+
+int main()
+{
+    //we declare two node
+    struct structnode *head = NULL;
+    struct structnode *second = NULL;
+
+    // we create memory space for the nodes
+    head = malloc(sizeof(struct structnode));
+    second = malloc(sizeof(struct structnode));
+
+    head->data = 45;// assigns data to the first node
+    head->next = second;//link first node to the second
+
+    second->data = 46;
+    second->next = NULL; //as second is the last node it will point to null
+
+    count_of_nodes(head);
+
+    return 0;
+//head->next
+//next will contain the link to the next node called second
+// head->next->next will contain NULL as it's the end of the node.
+
+
+
+}
